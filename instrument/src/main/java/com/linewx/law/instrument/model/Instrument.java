4f069c25a69f96b0f3fc00828b878b6afd9a9938
linewx <linewx1981@gmail.com>
2016-11-26 14:53:56 +0800

    refine code

--- instrument/src/main/java/com/linewx/law/instrument/model/Instrument.java
+++ instrument/src/main/java/com/linewx/law/instrument/model/Instrument.java
@@ -1,5 +1,8 @@
 package com.linewx.law.instrument.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.*;
 //import java.time.Instant;
 
@@ -15,8 +18,50 @@ public class Instrument implements java.io.Serializable {
 	@Column(unique = true)
 	private String email;
 
+	private static Logger logger = LoggerFactory.getLogger(Instrument.class);
 
-    protected Instrument() {
+	@Column
+	private String accuser;  //原告
+	private String accuserAlias; //原告别名
+	private String accuserLegalEntity;  //原告法人代表
+	private String accuserLawyer;  //原告律师
+	private String accuserLawyerOffice;  //原告律师的律师事务所
+	private String defendant;  //被告
+	private String defendantAlias; //被告别名
+	private String defendantLegalEntity;  //被告法人代表
+	private String defendantLawyer;  //被告律师
+	private String defendantLawyerOffice;  //被告律师的律师事务所
+	private String accuserProperty;  //原告企业类别和行业属性
+	private String defendantProperty;  //被告企业类别和行业属性
+	//private String judge;  //法官
+	private String mainJudge;  //主审法官
+	private String secondaryJudge;  //非主审法官
+	private String secondaryJudge2;  //非主审法官1
+	//private String secondaryJudge2;  //非主审法官2
+	private String date;  //判决日期
+	private String clerk;  //书记员
+	private String reason;  //案由
+	private String number;  //案号
+	private String caseType;  //案件类型
+	private String instrumentType;  //文书类型
+	private String suiteDate;  //立案年份
+	private String level;  //审级
+	private String court;  //法院
+	private Long cost;  //案件受理费
+	private Long amount;  //诉请金额
+	private Boolean ignoreAmount;  //诉请金额忽略标志
+	private Boolean discountHalf;  //受理费减半
+	private Long costOnDefendant;  //被告负担受理费
+	private Long costOnAccuser;  //原告负担受理费
+	private Long accuserWinPer;  //本案原告胜率
+	private Long defendantWinPer;  //本案被告胜率
+	private Long accuserAmount;  //原告主要诉请获支持金额
+	private Long accuserAmountPer;  //原告主要诉请获支持率
+	private Long defendantAmountPer;  //被告抗辩获支持率
+	private Boolean firstConciliation;  //一审调解结案
+
+
+    public Instrument() {
 
 	}
 	
@@ -28,7 +73,11 @@ public class Instrument implements java.io.Serializable {
 		return id;
 	}
 
-    public String getEmail() {
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
 		return email;
 	}
 
@@ -36,4 +85,299 @@ public class Instrument implements java.io.Serializable {
 		this.email = email;
 	}
 
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static void setLogger(Logger logger) {
+		Instrument.logger = logger;
+	}
+
+	public String getAccuser() {
+		return accuser;
+	}
+
+	public void setAccuser(String accuser) {
+		this.accuser = accuser;
+	}
+
+	public String getAccuserAlias() {
+		return accuserAlias;
+	}
+
+	public void setAccuserAlias(String accuserAlias) {
+		this.accuserAlias = accuserAlias;
+	}
+
+	public String getAccuserLegalEntity() {
+		return accuserLegalEntity;
+	}
+
+	public void setAccuserLegalEntity(String accuserLegalEntity) {
+		this.accuserLegalEntity = accuserLegalEntity;
+	}
+
+	public String getAccuserLawyer() {
+		return accuserLawyer;
+	}
+
+	public void setAccuserLawyer(String accuserLawyer) {
+		this.accuserLawyer = accuserLawyer;
+	}
+
+	public String getAccuserLawyerOffice() {
+		return accuserLawyerOffice;
+	}
+
+	public void setAccuserLawyerOffice(String accuserLawyerOffice) {
+		this.accuserLawyerOffice = accuserLawyerOffice;
+	}
+
+	public String getDefendant() {
+		return defendant;
+	}
+
+	public void setDefendant(String defendant) {
+		this.defendant = defendant;
+	}
+
+	public String getDefendantAlias() {
+		return defendantAlias;
+	}
+
+	public void setDefendantAlias(String defendantAlias) {
+		this.defendantAlias = defendantAlias;
+	}
+
+	public String getDefendantLegalEntity() {
+		return defendantLegalEntity;
+	}
+
+	public void setDefendantLegalEntity(String defendantLegalEntity) {
+		this.defendantLegalEntity = defendantLegalEntity;
+	}
+
+	public String getDefendantLawyer() {
+		return defendantLawyer;
+	}
+
+	public void setDefendantLawyer(String defendantLawyer) {
+		this.defendantLawyer = defendantLawyer;
+	}
+
+	public String getDefendantLawyerOffice() {
+		return defendantLawyerOffice;
+	}
+
+	public void setDefendantLawyerOffice(String defendantLawyerOffice) {
+		this.defendantLawyerOffice = defendantLawyerOffice;
+	}
+
+	public String getAccuserProperty() {
+		return accuserProperty;
+	}
+
+	public void setAccuserProperty(String accuserProperty) {
+		this.accuserProperty = accuserProperty;
+	}
+
+	public String getDefendantProperty() {
+		return defendantProperty;
+	}
+
+	public void setDefendantProperty(String defendantProperty) {
+		this.defendantProperty = defendantProperty;
+	}
+
+	public String getMainJudge() {
+		return mainJudge;
+	}
+
+	public void setMainJudge(String mainJudge) {
+		this.mainJudge = mainJudge;
+	}
+
+	public String getSecondaryJudge() {
+		return secondaryJudge;
+	}
+
+	public void setSecondaryJudge(String secondaryJudge) {
+		this.secondaryJudge = secondaryJudge;
+	}
+
+	public String getSecondaryJudge2() {
+		return secondaryJudge2;
+	}
+
+	public void setSecondaryJudge2(String secondaryJudge2) {
+		this.secondaryJudge2 = secondaryJudge2;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getClerk() {
+		return clerk;
+	}
+
+	public void setClerk(String clerk) {
+		this.clerk = clerk;
+	}
+
+	public String getReason() {
+		return reason;
+	}
+
+	public void setReason(String reason) {
+		this.reason = reason;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getCaseType() {
+		return caseType;
+	}
+
+	public void setCaseType(String caseType) {
+		this.caseType = caseType;
+	}
+
+	public String getInstrumentType() {
+		return instrumentType;
+	}
+
+	public void setInstrumentType(String instrumentType) {
+		this.instrumentType = instrumentType;
+	}
+
+	public String getSuiteDate() {
+		return suiteDate;
+	}
+
+	public void setSuiteDate(String suiteDate) {
+		this.suiteDate = suiteDate;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getCourt() {
+		return court;
+	}
+
+	public void setCourt(String court) {
+		this.court = court;
+	}
+
+	public Long getCost() {
+		return cost;
+	}
+
+	public void setCost(Long cost) {
+		this.cost = cost;
+	}
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public Boolean getIgnoreAmount() {
+		return ignoreAmount;
+	}
+
+	public void setIgnoreAmount(Boolean ignoreAmount) {
+		this.ignoreAmount = ignoreAmount;
+	}
+
+	public Boolean getDiscountHalf() {
+		return discountHalf;
+	}
+
+	public void setDiscountHalf(Boolean discountHalf) {
+		this.discountHalf = discountHalf;
+	}
+
+	public Long getCostOnDefendant() {
+		return costOnDefendant;
+	}
+
+	public void setCostOnDefendant(Long costOnDefendant) {
+		this.costOnDefendant = costOnDefendant;
+	}
+
+	public Long getCostOnAccuser() {
+		return costOnAccuser;
+	}
+
+	public void setCostOnAccuser(Long costOnAccuser) {
+		this.costOnAccuser = costOnAccuser;
+	}
+
+	public Long getAccuserWinPer() {
+		return accuserWinPer;
+	}
+
+	public void setAccuserWinPer(Long accuserWinPer) {
+		this.accuserWinPer = accuserWinPer;
+	}
+
+	public Long getDefendantWinPer() {
+		return defendantWinPer;
+	}
+
+	public void setDefendantWinPer(Long defendantWinPer) {
+		this.defendantWinPer = defendantWinPer;
+	}
+
+	public Long getAccuserAmount() {
+		return accuserAmount;
+	}
+
+	public void setAccuserAmount(Long accuserAmount) {
+		this.accuserAmount = accuserAmount;
+	}
+
+	public Long getAccuserAmountPer() {
+		return accuserAmountPer;
+	}
+
+	public void setAccuserAmountPer(Long accuserAmountPer) {
+		this.accuserAmountPer = accuserAmountPer;
+	}
+
+	public Long getDefendantAmountPer() {
+		return defendantAmountPer;
+	}
+
+	public void setDefendantAmountPer(Long defendantAmountPer) {
+		this.defendantAmountPer = defendantAmountPer;
+	}
+
+	public Boolean getFirstConciliation() {
+		return firstConciliation;
+	}
+
+	public void setFirstConciliation(Boolean firstConciliation) {
+		this.firstConciliation = firstConciliation;
+	}
 }
